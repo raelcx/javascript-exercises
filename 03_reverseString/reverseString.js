0e4b825c43858e5df90ea28f3d6b370ef32560aa
Israel Carvalho <77789295+raelcx@users.noreply.github.com>
2024-11-29 18:38:08 -0300

    Solve problems 1, 2 and 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringToArray = string.split('');
+    let arrayReversed = stringToArray.reverse();
+    let arrayBackToString = arrayReversed.join('');
+return arrayBackToString;
 };
 
 // Do not edit below this line
