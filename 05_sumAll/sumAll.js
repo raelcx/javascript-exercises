ec88cba8be6d029ec42addc47be5a2728ab9ca6d
Israel Carvalho <77789295+raelcx@users.noreply.github.com>
2024-11-30 13:29:20 -0300

    Solve exercises 4 to 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    let sum = 0;
+    let initialNumber;
+    let lastNumber;
 
+    if (number1 < 0 || number2 < 0
+        || Number.isInteger(number1) == false || Number.isInteger(number2) == false
+    ) {
+        return 'ERROR';
+    }
+
+    if (number1 > number2) {
+        initialNumber = number2;
+        lastNumber = number1;
+    } else {
+        initialNumber = number1;
+        lastNumber = number2;
+    }
+
+    for (let i = initialNumber; i <= lastNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = parseFloat(((fahrenheit - 32) * 5/9).toFixed(1));
+  
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = parseFloat(((celsius * 9/5) + 32).toFixed(1));
+
+  return fahrenheit;
 };
 
 // Do not edit below this line
