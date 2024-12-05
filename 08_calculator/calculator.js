54ecf0465fa6c62ab38d18ac44b382f0c84833b0
Israel Carvalho <77789295+raelcx@users.noreply.github.com>
2024-12-05 20:42:38 -0300

    Solve calculator problem

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,34 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  return arr.reduce((total, number) => total + number, 0);
 	
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((total, number) => total * number, 1);
 
 };
 
-const power = function() {
+const power = function(a, b) {
+  return a ** b;
 	
 };
 
-const factorial = function() {
+const factorial = function(number) {
+  let result = 1
+  for (let i = 1; i <= number; i++) {
+    result *= i;
+  }
+return result;
 	
 };
 
