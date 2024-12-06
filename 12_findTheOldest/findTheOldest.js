ae0a7cb6be36ae77b7a53ac39dc6350e1ae7e073
Israel Carvalho <77789295+raelcx@users.noreply.github.com>
2024-12-06 15:25:20 -0300

    Solve more exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,5 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.slice().sort((a, b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth))[0].name;
 };
 
 // Do not edit below this line
